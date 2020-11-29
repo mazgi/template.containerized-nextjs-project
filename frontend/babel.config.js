f24772b50e43663a61b37e800eee1972d5430e31
Hidenori Matsuki <dev@mazgi.com>
2020-11-29 07:27:21 +0000

    update libs

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,14 +1,14 @@
-{
-  "presets": ["next/babel"],
-  "plugins": [
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
     [
-      "module-resolver",
+      'module-resolver',
       {
-        "root": ["./src"],
+        root: ['./src'],
         // https://github.com/tleunen/babel-plugin-module-resolver/blob/a65c39a7e495328a9d4d70e82b567122d729e6cb/src/normalizeOptions.js#L11
         // defaultExtensions are '.js', '.jsx', '.es', '.es6', '.mjs'
-        "extensions": [".ts", ".tsx", ".js"]
-      }
-    ]
-  ]
+        extensions: ['.ts', '.tsx', '.js'],
+      },
+    ],
+  ],
 }
