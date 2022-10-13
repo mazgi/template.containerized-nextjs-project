096f04b7f81cc1d0497e496c2a35cbf11b679bfb
Hidenori Matsuki <dev@mazgi.com>
2022-10-13 16:55:27 +0000

    set up next.js and storybook

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  setupFiles: ['<rootDir>/jest.setup.ts']
+// See:
+//  - https://nextjs.org/docs/testing#jest-and-react-testing-library
+//  - https://github.com/vercel/next.js/blob/canary/examples/with-jest/jest.config.js
+
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  // https://github.com/testing-library/jest-dom#usage
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
 }
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
