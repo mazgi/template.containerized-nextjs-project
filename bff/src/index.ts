eb2524b8525cdbe82eb4122bc0c7d7ff92b8ee7c
Hidenori Matsuki <dev@mazgi.com>
2022-10-13 16:59:15 +0000

    simplify bff

--- bff/src/index.ts
+++ bff/src/index.ts
@@ -1,24 +1,14 @@
-import Config from 'config'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
+// import cors from 'cors'
 import express from 'express'
 
 const start = async (): Promise<void> => {
-  const config = await Config.getConfig()
-
   const app = express()
-  app.set('port', config.server.port)
-  // CORS
-  app.use(
-    cors({
-      origin: config.server.origins,
-      credentials: !config.server.origins.includes('*'),
-    })
-  )
-  // Parse cookie for authentication
+  app.set('port', 4000)
   app.use(cookieParser())
 
   app.listen(app.get('port'), () => {
+    app.get('/api/status', (req, res) => res.json({}))
     console.log(
       `⚡ App is running at :%d in %s mode`,
       app.get('port'),
