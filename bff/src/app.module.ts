bcb7d03ce1bf68198126a5c19c2aecfe52b71aa9
Hidenori Matsuki <dev@mazgi.com>
2022-10-18 14:15:22 +0000

    implement status

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { RouterModule } from '@nestjs/core'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { StatusModule } from './rest/status/status.module'
 
 @Module({
-  imports: [],
+  imports: [
+    StatusModule,
+    // https://docs.nestjs.com/recipes/router-module
+    RouterModule.register([
+      {
+        path: 'rest',
+        children: [StatusModule],
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
