6e0e61a78705683fe9c246cdbe1a397d10f79f80
Hidenori Matsuki <dev@mazgi.com>
2022-10-18 10:38:59 +0000

    recreate bff

--- bff/src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-describe('App', () => {
-  test('should be the same port to config', async () => {
-    const app = express()
-    app.set('port', 4000)
-    expect(app.get('port')).toBe(4000)
-  })
-})
