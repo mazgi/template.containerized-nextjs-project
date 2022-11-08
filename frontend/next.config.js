3db77ffe3f168ae143510090f4fa1bd3b2a1fc53
Hidenori Matsuki <dev@mazgi.com>
2022-11-08 14:05:00 +0000

    remove data-test* in production.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    // https://nextjs.org/docs/advanced-features/compiler#remove-react-properties
+    reactRemoveProperties: true,
+  },
   // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
   // > Next.js can automatically create a standalone folder that copies only the necessary files for a production deployment including select files in node_modules.
   output: 'standalone',
