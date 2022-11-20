07290cbcbd08438eca7a37a9b5e237fc6450ac3f
Hidenori Matsuki <dev@mazgi.com>
2022-11-20 14:10:54 +0000

    update the view using the api client

--- frontend/pages/status/index.tsx
+++ frontend/pages/status/index.tsx
@@ -1,16 +1,48 @@
+import aspida from '@aspida/fetch'
+import useAspidaSWR from '@aspida/swr'
 import { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import api from '~/lib/client/rest/$api'
 import { Status } from '~/pages/api/status'
 import styles from '~/styles/Status.module.css'
 
+const BFFStatus: React.FC = () => {
+  const client = api(
+    aspida(fetch, {
+      baseURL:
+        process.env.NEXT_PUBLIC_BFF_ENDPOINT_REST_BASE_PATH || 'undef.local',
+    })
+  )
+  const { data, error } = useAspidaSWR(client.rest.status)
+  if (error)
+    return (
+      <Fragment>
+        <p>Failed to load</p>
+      </Fragment>
+    )
+  if (!data)
+    return (
+      <Fragment>
+        <p>Loading...</p>
+      </Fragment>
+    )
+  return (
+    <Fragment>
+      <dl>
+        <dt>name:</dt>
+        <dd>{data.name}</dd>
+        <dt>state:</dt>
+        <dd>{data.state}</dd>
+        <dt>version:</dt>
+        <dd>{data.version}</dd>
+      </dl>
+    </Fragment>
+  )
+}
+
 const Page: NextPage = () => {
   const [status, setStatus] = useState<Status>({
-    name: 'initial name',
-    state: 'initial state',
-    version: '0.0.0+initial',
-  })
-  const [statusBff, setStatusBff] = useState<Status>({
     name: 'initial name',
     state: 'initial state',
     version: '0.0.0+initial',
@@ -25,17 +57,6 @@ const Page: NextPage = () => {
     fetchStatus()
   }, [])
 
-  useEffect(() => {
-    const fetchStatus = async () => {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BFF_ENDPOINT_REST}/status`
-      )
-      const json = await res.json()
-      setStatusBff(json)
-    }
-    fetchStatus()
-  }, [])
-
   return (
     <div>
       <Head>
@@ -46,15 +67,18 @@ const Page: NextPage = () => {
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2>Frontend Status</h2>
-            <p>name: {status.name}</p>
-            <p>state: {status.state}</p>
-            <p>version: {status.version}</p>
+            <dl>
+              <dt>name:</dt>
+              <dd>{status.name}</dd>
+              <dt>state:</dt>
+              <dd>{status.state}</dd>
+              <dt>version:</dt>
+              <dd>{status.version}</dd>
+            </dl>
           </div>
           <div className={styles.card}>
             <h2>BFF Status</h2>
-            <p>name: {statusBff.name}</p>
-            <p>state: {statusBff.state}</p>
-            <p>version: {statusBff.version}</p>
+            <BFFStatus></BFFStatus>
           </div>
         </div>
       </main>
