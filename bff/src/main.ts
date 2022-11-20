4f48150438771661b0407d69a336c47f46b692bf
Hidenori Matsuki <dev@mazgi.com>
2022-11-20 12:35:56 +0000

    enable swagger ui

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from '~/src/app.module'
 
 async function bootstrap() {
@@ -7,6 +8,15 @@ async function bootstrap() {
     origin: process.env.ALLOW_ORIGIN,
     credentials: true,
   })
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle(`${process.env.npm_package_name || 'bff+undef'} REST Web API`)
+      .setDescription('The Swagger UI')
+      .setVersion(process.env.npm_package_version || '0.0.0+undef')
+      .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('swagger', app, document)
+  }
   const port = Number(process.env.PORT) || 4000
   await app.listen(port)
 }
