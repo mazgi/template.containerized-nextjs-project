93bd51bf444a66aa77bee82bed15a46fc6150ea1
Hidenori Matsuki <dev@mazgi.com>
2022-11-20 12:40:01 +0000

    add openapi anotations

--- bff/src/rest/status/entities/status.entity.ts
+++ bff/src/rest/status/entities/status.entity.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class Status {
+  @ApiProperty({
+    example: 'bff',
+    description: 'The service name of BFF',
+  })
   name: string
+
+  @ApiProperty({
+    example: 'healthy',
+    description: 'The state of BFF',
+  })
   state: 'healthy' | 'unhealthy'
+
+  @ApiProperty({
+    example: '1.2.3',
+    description: 'The semantic version of BFF',
+  })
   version: string
 }
