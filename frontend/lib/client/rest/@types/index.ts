869708ce4f1db1eccdf9130a33804eb1f984ae34
Hidenori Matsuki <dev@mazgi.com>
2022-11-20 13:54:06 +0000

    generate the openapi client

---
+++ frontend/lib/client/rest/@types/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export type Status = {
+  /** The service name of BFF */
+  name: string
+  /** The state of BFF */
+  state: string
+  /** The semantic version of BFF */
+  version: string
+}
