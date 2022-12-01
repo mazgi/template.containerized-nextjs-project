eb927780038f9e5912868eca5f50ef3499e0e7b9
Hidenori Matsuki <dev@mazgi.com>
2022-12-01 23:43:16 +0000

    set up graphql endpoint

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -1,9 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
+import { GraphQLModule } from '@nestjs/graphql'
 import { StatusModule as RESTStatusModule } from '~/src/rest/status/status.module'
 
 @Module({
   imports: [
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: true,
+    // }),
     RESTStatusModule,
     // https://docs.nestjs.com/recipes/router-module
     RouterModule.register([
