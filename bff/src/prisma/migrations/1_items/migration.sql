974afdb1c45e66177bc3146d2dd116501cc2ff3e
Hidenori Matsuki <dev@mazgi.com>
2022-12-03 03:51:34 +0000

    create a table

---
+++ bff/src/prisma/migrations/1_items/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `items` (
+    `id` VARCHAR(255) NOT NULL,
+    `text` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
